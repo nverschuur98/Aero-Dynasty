d583424882e781637a25e5cf2c2d18bfcc17e756
Nick <n.verschuur98@gmail.com>
2025-01-10 23:34:56 +0100

    Added Country Model

---
+++ AeroDynasty.Core/Enums/Continents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeroDynasty.Core.Enums
+{
+    public enum Continent
+    {
+        Africa,
+        Antarctica,
+        Asia,
+        Europe,
+        NorthAmerica,
+        SouthAmerica,
+        Oceania
+    }
+}
---
+++ AeroDynasty.Core/Models/Core/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AeroDynasty.Core.Enums;
+
+namespace AeroDynasty.Core.Models.Core
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string ISOCode { get; set; }
+        public Continent Continent { get; set; }
+        public string FlagPath
+        {
+            get
+            {
+                return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Flags", $"{ISOCode}.png");
+            }
+        }
+    }
+}
